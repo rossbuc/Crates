bf96f1cfe4eab92005f92a512ec2809515ff22b7
Ross Buchan <rossbuchan1@googlemail.com>
2024-03-09 15:31:02 +0100

    Write preload api for renderer

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,22 +1,34 @@
-import { contextBridge } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
+import { contextBridge, ipcRenderer } from "electron";
+import { electronAPI } from "@electron-toolkit/preload";
 
 // Custom APIs for renderer
-const api = {}
+const api = {
+  openFile: (): Promise<string | undefined> => {
+    return ipcRenderer.invoke("dialog:openFile");
+  },
+  fileExplorer: (): Promise<string[] | undefined> => {
+    console.log("Preloading the contextBridge baby");
+    return ipcRenderer.invoke("dialog:fileExplorer");
+  },
+  loadLib: (): Promise<JSON> => {
+    console.log("This is from inside the loadLib preload api");
+    return ipcRenderer.invoke("loadLib");
+  },
+};
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld("electron", electronAPI);
+    contextBridge.exposeInMainWorld("api", api);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 } else {
   // @ts-ignore (define in dts)
-  window.electron = electronAPI
+  window.electron = electronAPI;
   // @ts-ignore (define in dts)
-  window.api = api
+  window.api = api;
 }
